bd6ec6bf80f13fc34660029d3d6c1b19030e2cad
Christian Zwaz <christian.zwaz@gmail.com>
2020-10-08 23:24:22 +0200

    serial_RS232_example: changed RX-FSM clk input to 480 KHz.

--- quartus_ws/serial_RS232_example/serial_test.vhd
+++ quartus_ws/serial_RS232_example/serial_test.vhd
@@ -7,19 +7,23 @@ use ieee.std_logic_1164.all;
 ENTITY serial_test IS
 	GENERIC (
 		CLK_FREQ		:	INTEGER	:= 48000000;
+		CLK_DIV		:	INTEGER	:= 100;
 		BAUD_RATE	:	INTEGER	:= 9600
 		);
 		
 	PORT (
 		rx_pl2303	:	OUT	std_logic; -- TX-FPGA --> RX-PL2303
 		tx_pl2303	:	IN		std_logic; -- RX-FPGA <-- TX-PL2303
-		clk			:	IN		std_logic;
+		clk			:	IN		std_logic; -- 48MHz
 		rst			:	IN		std_logic;
 		
 		data			:	INOUT	std_logic_vector ( 7 downto 0 );
 		data_valid	:	OUT	std_logic;
 		
-		led_vec		:	OUT	std_logic_vector ( 3 downto 0 )
+		led_vec		:	OUT	std_logic_vector ( 3 downto 0 );
+		hsync			:	OUT	std_logic;
+		vsync			:	INOUT	std_logic;
+		debug			:	OUT	std_logic_vector ( 2 downto 0 )
 		);
 END ENTITY serial_test;
 -------------------------------------------------------------------------------
@@ -33,8 +37,9 @@ ARCHITECTURE behaviour OF serial_test IS
 								);
 	SIGNAL rx_fsm_state	:	RX_FSM_TYPE	:=	IDLE;
 	SIGNAL rx_fsm_state_next	:	RX_FSM_TYPE;
+	SIGNAL clk_480KHz		:	std_logic := '0';
 	
-	CONSTANT	T2_cnt_top	:	INTEGER := CLK_FREQ / BAUD_RATE;
+	CONSTANT	T2_cnt_top	:	INTEGER := CLK_FREQ / CLK_DIV / BAUD_RATE;
 	CONSTANT T1_cnt_top	:	INTEGER := 3 * T2_cnt_top / 2;
 	CONSTANT RX_IDLE_VAL	:	std_logic := '1';
 	CONSTANT	STOP_VAL		:	std_logic := '1';
@@ -42,23 +47,37 @@ ARCHITECTURE behaviour OF serial_test IS
 
 BEGIN
 	rx_pl2303 <= tx_pl2303; -- for debugging only!!!
+	hsync	<=	tx_pl2303;		-- for debugging only!!!
+	
+	-- Clock divider: 48MHz --> 480KHz
+	CLK_DIVIDER: PROCESS (clk, rst)
+		VARIABLE clk_cnt : INTEGER RANGE 0 TO 50;
+	BEGIN
+		IF rst = '0' THEN
+			clk_cnt := 0;
+		ELSIF rising_edge(clk) THEN
+			IF clk_cnt = 50-1 THEN
+				clk_cnt := 0;
+				clk_480KHz <= not clk_480KHz;
+				VSYNC <= not VSYNC;
+			ELSE
+				clk_cnt := clk_cnt + 1;
+			END IF;
+		END IF;
+	END PROCESS;
+		
+	
 	-- NEXT STATE PROCESS
-	PROCESS (clk, rst)
+	PROCESS (clk_480KHz, rst)
 		VARIABLE cnt : INTEGER RANGE 0 TO T1_cnt_top;
 		VARIABLE BIT_cnt : INTEGER RANGE 0 TO NUM_OF_BITS;
 	
 	BEGIN
-	
 		IF rst = '0' THEN
 			rx_fsm_state_next <= IDLE;
-			--data_valid <= '0';
-			--data <= (others => '0');
-			--led_vec <= (others => '1');
 			
-		ELSIF rising_edge (clk) THEN
-		
-			-- by default stay in current state
-			rx_fsm_state_next <= rx_fsm_state;
+		ELSIF rising_edge (clk_480KHz) THEN
+			rx_fsm_state_next <= rx_fsm_state;	-- by default stay in current state
 			
 			CASE rx_fsm_state IS
 				
@@ -70,7 +89,7 @@ BEGIN
 					END IF;
 					
 				WHEN WAIT_T1 =>
-					IF cnt = T1_cnt_top THEN
+					IF cnt = T1_cnt_top-1 THEN
 						rx_fsm_state_next <= SAMPLE;
 						BIT_cnt := 0;
 					ELSE
@@ -84,7 +103,7 @@ BEGIN
 					cnt := 0;
 					
 				WHEN WAIT_T2 =>
-					IF cnt = T2_cnt_top THEN
+					IF cnt = T2_cnt_top-2 THEN
 						IF BIT_cnt = NUM_OF_BITS THEN
 							rx_fsm_state_next <= SAMPLE_STOP_SET_OUTPUT;
 						ELSE
@@ -111,12 +130,25 @@ BEGIN
 	PROCESS (rx_fsm_state)
 	BEGIN
 		CASE rx_fsm_state IS
+		
+			WHEN IDLE =>
+				debug <= "000";
+				
+			WHEN WAIT_T1 =>
+				debug <= "001";
+		
+			WHEN SAMPLE =>
+				debug <= "010";
+				
+			WHEN WAIT_T2 =>
+				debug <= "011";
 
 			WHEN SAMPLE_STOP_SET_OUTPUT =>
 				led_vec <= not data ( 3 downto 0 );
+				debug <= "100";
 				
 			WHEN OTHERS =>
-				
+				debug <= "111";
 				
 		END CASE;
 	END PROCESS;
