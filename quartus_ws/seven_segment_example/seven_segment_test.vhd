570ee51a93cd448fc7e420aa37e45afd2b9389d3
Christian Zwaz <christian.zwaz@gmail.com>
2020-10-10 23:56:07 +0200

    seven segment - continued work on the project.

--- quartus_ws/seven_segment_example/seven_segment_test.vhd
+++ quartus_ws/seven_segment_example/seven_segment_test.vhd
@@ -1,51 +1,90 @@
 library ieee ;
 use ieee.std_logic_1164.all;
+use ieee.numeric_std.all; 
 
 -- author: CZW
--- date: 20200930
+-- date: 20200930 / 20201009
 
 entity seven_segment_test is
 	port (
-		clk : in std_logic;
+		clk	: in std_logic;
+		rst	: in std_logic;
 		
-		SW : in std_logic_vector (3 downto 0);
+		LEDs		: out std_logic_vector (0 to 7);
+		led_pos	: out std_logic_vector (3 downto 0);
 		
-		LEDs : out std_logic_vector (0 to 7);
-		
-		DIG1 : out std_logic;
-		DIG2 : out std_logic;
-		DIG3 : out std_logic;
-		DIG4 : out std_logic
+		debug1	: out std_logic;
+		debug2	: out std_logic
 	);
 end entity seven_segment_test;
 
 
 architecture behaviour of seven_segment_test is
-begin
-	process (clk)
+
+	TYPE state_type IS (shift_digit, hold_output);
+	SIGNAL fsm_state	:	state_type := shift_digit;
+	SIGNAL fsm_state_next	: state_type;
+	SIGNAL led_temp : std_logic_vector (3 downto 0) := "0001";
+	SIGNAL temp : std_logic := '0';
+	SIGNAL temp2 : std_logic := '0';
 	
-		variable clk_cnt : integer := 0;
-		variable char : std_logic_vector (0 to 7) := "10000000";
+	CONSTANT hold_cnt_top	: INTEGER := 480000;
+	
+begin
+
+	process (clk, rst)
+	
+		variable hold_cnt : integer := 0;
 	
 	begin
-	
-		DIG1 <= '0';
-		DIG2 <= '0';
-		DIG3 <= '0';
-		DIG4 <= '0';
-	
-		if clk'event and clk = '1' then
 		
-			if clk_cnt = 48000000/2 then
-				clk_cnt := 0;
-			else
-				clk_cnt := clk_cnt + 1;
-			end if;
+		if rst = '0' then
+			fsm_state_next <= shift_digit;
 			
-			LEDs <= char;
-	
+		elsif rising_edge(clk) then
+			fsm_state_next <= fsm_state;
+			
+			case fsm_state is
+				when shift_digit =>
+					fsm_state_next <= hold_output;
+					
+				when hold_output =>
+					if hold_cnt = hold_cnt_top then
+						hold_cnt := 0;
+						fsm_state_next <= shift_digit;
+						temp2 <= not temp2;
+					else
+						hold_cnt := hold_cnt + 1;
+					end if;
+			end case;
 		end if;
 		
+		fsm_state <= fsm_state_next;
+		debug2 <= temp2;
+		
 	end process;
+	
+	PROCESS (fsm_state)
+	
+		variable char : std_logic_vector (0 to 7) := "11110000";
+		
+	BEGIN
+		case fsm_state is
+		
+			when shift_digit =>
+				LEDs <= char;
+				led_temp <= led_temp(0) & led_temp(3 downto 1);
+				led_pos <= "0101";
+				temp <= not temp;
+				--LEDs <= "10101010";--
+						
+			when hold_output =>
+				--LEDs <= "01010101";
+			
+		end case;
+		
+		debug1 <= temp;
+	end process;
+			
 
 end architecture behaviour;
