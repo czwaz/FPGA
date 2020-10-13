8327e28df5d19ea9802363c402f02cec1e6e4022
Christian Zwaz <christian.zwaz@gmail.com>
2020-10-14 01:11:37 +0200

    project "memory_example" - initial commit.

---
+++ project_workspace/memory_example/memory.vhd
@@ -0,0 +1,19 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
+
+-- author: CZW
+-- date: 20201014
+
+entity memory is
+    port (
+        
+        );
+end entity;
+
+architecture behaviour of memory is
+
+
+begin
+
+end architecture;
